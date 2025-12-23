ed61ca04ba9596fc10a40f8c04fdd46a9a0a1519
Arjunlama2 <instructorarjun1@gmail.com>
2025-12-23 19:02:07 +0545

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-
 function Card({ data }) {
-
-    return (
-        <div className='border-8'>
-        <div className='h-[236px]'>
-            <div className='bg-[#F6F7FB] p-[45px] flex justify-center items-center '>
-                <img src={data.image} alt="" height={175} width={175} className='h-[175px] w-[175px]' />
-            </div >
-            <div className='flex items-center justify-center flex-col gap-6  bg-accent '>
-                <p className='text-[#FB2E86] font-bold'>{data.title}</p>
-                <p className='text-[#151875]'>Code - Y523201</p>
-                <p className='text-[#151875]'>$42.00</p>
-            </div>
-</div>
+  return (
+    <div className="w-[270px] bg-white shadow-md ">
+      <div className="h-[236px]">
+        <div className="bg-[#F6F7FB] p-[45px] flex justify-center items-center">
+          <img
+            src={data.image}
+            alt=""
+            className="h-[175px] w-[175px]"
+          />
         </div>
-    )
+
+       
+      </div>
+       <div className="flex flex-col items-center gap-2 py-4">
+          <p className="text-[#FB2E86] font-bold">{data.title}</p>
+          <p className="text-[#151875]">Code - Y523201</p>
+          <p className="text-[#151875]">$42.00</p>
+        </div>
+    </div>
+  );
 }
 
 export default Card
